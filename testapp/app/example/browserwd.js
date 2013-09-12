f34f548f7b97a8df492016ab508b71ebd990615e
Julie <ju.ralph@gmail.com>
2013-09-12 12:46:08 -0700

    Oops deleted a line.

--- testapp/app/example/browserwd.js
+++ testapp/app/example/browserwd.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.(function(exports){var COMPILED = !0, goog = goog || {};
-
+(function(exports){var COMPILED = !0, goog = goog || {};
 goog.global = this;
 goog.exportPath_ = function(a, b, c) {
   a = a.split(".");
